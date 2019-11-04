b3c08d0d7ef702011010f23326cb9463e0e4c23c
RikBrito <riksbrito@gmail.com>
2019-11-04 20:52:08 -0300

    reboot app

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,24 +14,59 @@ import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    
-    <View backgroundimage="../src/imagens/Fundo 1.png" style={{ paddingVertical: 20 }}>
-    <Card>
-      <FormLabel>E-mail</FormLabel>
-      <FormInput placeholder="Digite seu e-mail" />
-      <FormLabel>Senha</FormLabel>
-      <FormInput secureTextEntry placeholder="Digite sua senha" />
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
 
-      <Button
-        buttonStyle={{ marginTop: 20 }}
-        backgroundColor="#03A9F4"
-        title="Entrar"
-        onPress={() => {
-        onSignIn().then(() => navigation.navigation("SignedIn"));
-        }}
-      />
-    </Card>
-  </View> );
+        <View style={styles.getStartedContainer}>
+          <DevelopmentModeNotice />
+
+          <Text style={styles.getStartedText}>Get started by opening</Text>
+
+          <View
+            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+            <MonoText>screens/HomeScreen.js</MonoText>
+          </View>
+
+          <Text style={styles.getStartedText}>
+            Change this text and your app will automatically reload.
+          </Text>
+        </View>
+
+        <View style={styles.helpContainer}>
+          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+            <Text style={styles.helpLinkText}>
+              Help, it didn’t automatically reload!
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      <View style={styles.tabBarInfoContainer}>
+        <Text style={styles.tabBarInfoText}>
+          This is a tab bar. You can edit it in:
+        </Text>
+
+        <View
+          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+          <MonoText style={styles.codeHighlightText}>
+            navigation/MainTabNavigator.js
+          </MonoText>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 HomeScreen.navigationOptions = {
@@ -60,6 +95,19 @@ function DevelopmentModeNotice() {
     );
   }
 }
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/development-mode/'
+  );
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
