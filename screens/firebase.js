81375482cc3a905bc6e37fe34769b8dbd23f888f
Filipe Alm <filipe.engsft@gmail.com>
2019-11-15 17:03:49 -0300

    ]

---
+++ screens/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+Config = {
+    apiKey: "AIzaSyASr0XuvRC2K_btO_MN7J9mBJiC386Ctao",
+    authDomain: "my-dog-app-36889.firebaseapp.com",
+    databaseURL: "https://my-dog-app-36889.firebaseio.com",
+    projectId: "my-dog-app-36889",
+    storageBucket: "my-dog-app-36889.appspot.com",
+    messagingSenderId: "815535862651",
+    appId: "1:815535862651:web:37df0b50722f0b4e0283de"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  export default firebase;
